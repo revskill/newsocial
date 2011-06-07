0168cfe9c81d666b7338508b9eca57c453b33191
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-06-07 17:29:25 +0700

    User signup complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsocial::Application.config.secret_token = '80d0f1f8b6b50a6ea2f75608a5c56309d842bfec08545f4452348a7fca8c4dd42537758b87eb33ccf30855f1c73d7415c66491b2bb60da23d53ef8aace96aab0'
